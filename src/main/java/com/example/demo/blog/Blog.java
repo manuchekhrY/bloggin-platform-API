ec9e7da4729dd0d47bcfb6082b63ce305d5cf9b6
manuchekhrY <myakh2021@gmail.com>
2024-11-12 22:04:34 +0100

    filling Blog class and more

---
+++ src/main/java/com/example/demo/blog/Blog.java
@@ -0,0 +1,97 @@
+package com.example.demo.blog;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+public class Blog {
+
+    @Id
+    @SequenceGenerator(
+            name = "blog_sequence",
+            sequenceName = "blog_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "blog_sequence"
+    )
+    private Long id;
+    private String title;
+    private String content;
+    private String category;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Blog() {
+    }
+
+    public Blog(String title, String content, String category) {
+        this.title = title;
+        this.content = content;
+        this.category = category;
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return LocalDateTime.now();
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Blog{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", category='" + category + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/demo/blog/BlogService.java
@@ -0,0 +1,22 @@
+package com.example.demo.blog;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BlogService {
+
+    private final BlogRepository blogRepository;
+
+    @Autowired
+    public BlogService(BlogRepository blogRepository) {
+        this.blogRepository = blogRepository;
+    }
+
+    public List<Blog> getBlogs() {
+        return blogRepository.findAll();
+    }
+
+}
