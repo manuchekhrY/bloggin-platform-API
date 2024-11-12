bb965420f8936c5c8742c9011b6ea1f57b93cf04
manuchekhrY <myakh2021@gmail.com>
2024-11-12 22:20:23 +0100

    connecting to database

---
+++ src/main/java/com/example/demo/blog/BlogController.java
@@ -0,0 +1,26 @@
+package com.example.demo.blog;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/posts")
+public class BlogController {
+
+    private final BlogService blogService;
+
+    @Autowired
+    public BlogController(BlogService blogService) {
+        this.blogService = blogService;
+    }
+
+    @GetMapping
+    public List<Blog> getBlogs() {
+        return blogService.getBlogs();
+    }
+
+}
